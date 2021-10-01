f83d4ab87fbf1d4991ea697d2c467d056f3396e3
Swarag-N <swaragcrashing@gmail.com>
2021-10-02 00:08:58 +0530

    Add Email Authentication

--- server/colibri_server/settings.py
+++ server/colibri_server/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'Authentication.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'Authentication',
     
     'rest_framework',
     'posts',
