86caef40463bf4ab013368ff851c1b3bc934ee9a
Swarag-N <swaragcrashing@gmail.com>
2021-10-02 20:20:39 +0530

    Update DB to Postgres

--- server/colibri_server/settings.py
+++ server/colibri_server/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
+import environ
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env = environ.Env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -77,12 +81,23 @@ WSGI_APPLICATION = 'colibri_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':env('DB_NAME'),
+      'USER':env('DB_USER'),
+      'PASSWORD':env('DB_PASS'),
+      'HOST':env('DB_HOST'),
+      'PORT':env('DB_PORT'),
+   }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
